7ef9433b147e4f845a7cd8ba42079cde9d82b133
Sky Man King <skymankingtam@gmail.com>
2021-12-26 09:17:28 +0700

    Done update data to report

--- TEST.py
+++ TEST.py
@@ -12,17 +12,28 @@ import xlwings as xw
 
 #=================== Time OT =========================
 
-data_convert = openpyxl.load_workbook('baocao1.xlsx')
+
+
+baocao_1 = xlrd.open_workbook('baocao.xlsx')
+data_baocao = baocao_1.sheet_by_index(0)
+
+# BAO CAO
+all_rows_baocao = []
+for row in range(7 ,data_baocao.nrows):
+    curr_row = []
+    for col in range(data_baocao.ncols):
+        curr_row.append(data_baocao.cell_value(row, col))
+    all_rows_baocao.append(curr_row)
+
+
+data_convert = openpyxl.load_workbook('Template_report.xlsx')
 sheet_name_data_convert = data_convert.sheetnames[0]
 sh_data_convert = data_convert[sheet_name_data_convert]
 
 rows_data_convert= sh_data_convert.max_row #9
 cols_data_convert = sh_data_convert.max_column #9
 
-print(rows_data_convert, cols_data_convert)
-
-for row_convert in range(1, rows_data_convert + 1):
-    for col_convert in range(1, cols_data_convert):
-        print()
-
-
+for row in range(1, len(all_rows_baocao)+1):
+    for col in range(1, len(all_rows_baocao[0])+1):
+        sh_data_convert.cell(row+10, col).value = all_rows_baocao[row-1][col-1]
+data_convert.save("test1.xlsx")
